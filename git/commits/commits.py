08a8c84d4ea6e373901a6eb99e6c617f8fdcc0d4
Nathan <n49874687@gmail.com>
2025-09-03 17:52:11 +0200

    feat : initialisation contenu commits

--- git/commits/commits.py
+++ git/commits/commits.py
@@ -1,7 +1,13 @@
-from git.trees.tree import Tree
-from git.utils import get_last_commit
+import zlib
+from hashlib import sha1
+import time
 
-author = "test"
+from git.trees.tree import Tree
+# from git.utils import get_last_commit
+
+author = "Test Name"
+email = "Test@example.fr"
+message = "Test Message"
 
 class Commit:
     """Contains the current tree, the parent tree, the author, the date and the message"""
@@ -9,6 +15,27 @@ class Commit:
     def __init__(self, file_path, repo_path):
         self.file_path = file_path
         self.repo_path = repo_path
-    
+        content = self._get_content()
+        header = f"commit {len(content)}\0".encode()
+        content = header + content.encode()
+        self.sha1 = sha1(content)
+        self.encoded_content = zlib.compress(content)
+
+    def _get_content(self) -> str:
+        """Returns the content of the commit file"""
+        current_tree = Tree(self.file_path, self.repo_path)
+        timestamp = int(time.time())
+        timezone = time.strftime("%z")
+        
+        lines = [
+            f"tree {current_tree.sha1}",
+            f"author {author} <{email}> {timestamp} {timezone}",
+            f"commiter {author} <{email}> {timestamp} {timezone}",
+            "",
+            message
+        ]
+
+        return "\n".join(lines)
+
 def main():
     pass
